df14b81e0102daaf1214b614ee525eb6885ac3bc
Jesse <25578668+sora4222@users.noreply.github.com>
2025-11-07 16:07:47 +1100

    chore: prettier formatting

--- spaced-repetition/app/components/question-generator.tsx
+++ spaced-repetition/app/components/question-generator.tsx
@@ -2,5 +2,17 @@ import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardHeader } from "./ui/card";
 
 export function QuestionGenerator() {
-  return (<main className="flex min-h-svh w-full items-center justify-center"><Card className="px-4 container my-1"><CardHeader>Generate Question</CardHeader><CardDescription>A question about your learnings will be generated.</CardDescription><CardContent className="items-center"><Button variant="default">Generate</Button></CardContent></Card></main>)
+  return (
+    <main className="flex min-h-svh w-full items-center justify-center">
+      <Card className="px-4 container my-1">
+        <CardHeader>Generate Question</CardHeader>
+        <CardDescription>
+          A question about your learnings will be generated.
+        </CardDescription>
+        <CardContent className="items-center">
+          <Button variant="default">Generate</Button>
+        </CardContent>
+      </Card>
+    </main>
+  );
 }
