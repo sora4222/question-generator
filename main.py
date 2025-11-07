6e5df3959775078ef1b5d298fcc5861c90017ac8
Jesse <25578668+sora4222@users.noreply.github.com>
2025-11-07 15:28:55 +1100

    feat: adds debugging ability

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/healthz")
 def healthz():
     return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
