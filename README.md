b6954197e9454bd239519bafb361b7a0194dcba9
Jesse <25578668+sora4222@users.noreply.github.com>
2025-11-07 15:19:51 +1100

    feat: adds api

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Question Generator
 
 Generates questions from the information that the user provides.
+
+Run the API with the command:
+
+```bash
+fastapi dev main.py
+```
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from langchain.chat_models import init_chat_model
-from langchain_core.prompts import ChatPromptTemplate
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def main():
-    """Entry."""
-    template = "Generate a question and it's answer that can be used to test a student's knowledge of the following topic: {topic}"
-    prompt = ChatPromptTemplate.from_template(template)
-    llm = init_chat_model("llama3.2:3b", model_provider="ollama", temperature=0.2)
-    chain = prompt | llm
-    response = chain.invoke({"topic": "Python programming"})
-    print(response)
-
-
-if __name__ == "__main__":
-    main()
+@app.get("/healthz")
+def healthz():
+    return {"status": "healthy"}
